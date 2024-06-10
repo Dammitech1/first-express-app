b20eebec4994862ca16b732adf5234b34b154ae2
Dammitech1 <muhammed.m2302994@st.futminna.edu.ng>
2024-06-10 04:28:17 -0700

    app.get/post

--- dammi.js
+++ dammi.js
@@ -4,23 +4,33 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-
-app.get('/groceries', (req, res) => {
-    res.send([
-        {
-            item: 'milk',
-            quantity: 5,
-        },
-        {
-            item: 'egg',
-            quantity: 10,
-         },
-         {
-            item: 'baking-powder',
-            quantity: 15,
-         },
-
-    ]);
-});
+app.use(express.json());
 
 app.listen(port, () => console.log(`running express on server port ${port}`));
+
+
+
+const grocerylist = [
+    {
+        item: 'milk',
+        quantity: 5,
+    },
+    {
+        item: 'egg',
+        quantity: 10,
+     },
+     {
+        item: 'baking-powder',
+        quantity: 15,
+     },
+
+]
+app.get('/groceries', (req, res) => {
+    res.send(grocerylist);
+
+});
+
+app.post('/groceries', (req, res) => {
+    console.log(req.body);
+    res.send(200);
+});
