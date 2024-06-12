72b94548ec247d7fdc2a32dcdc360f60040ba692
Dammitech1 <muhammed.m2302994@st.futminna.edu.ng>
2024-06-12 05:57:56 -0700

    introduc about G,PO,PA,DE

--- dammi.js
+++ dammi.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.listen(port, () => console.log(`running express on server port ${port}`));
 
@@ -23,14 +24,38 @@ const grocerylist = [
         item: 'baking-powder',
         quantity: 15,
      },
-
+     {
+        item: 'car',
+        quantity: 4,
+    },
 ]
+
 app.get('/groceries', (req, res) => {
     res.send(grocerylist);
-
 });
 
 app.post('/groceries', (req, res) => {
-    console.log(req.body);
-    res.send(200);
+    const grocery = req.body
+    grocerylist.push(grocery)
+    res.send(grocerylist);
 });
+
+app.patch('/groceries', (req, res) =>{
+    const grocery = req.body
+    grocerylist.find((thisGrocery, index) => {
+        if (thisGrocery.item === grocery.item) {
+            grocerylist[index].quantity = grocery.quantity
+        }
+    } )
+    res.send(grocerylist);
+})
+
+app.delete('/groceries', (req, res) => {
+    const grocery = req.body
+    grocerylist.find((thisgrocery, index) => {
+        if (thisgrocery.item === grocery.item) {
+            grocerylist.splice(index, 1)
+        }
+    })
+    res.send(grocerylist)
+})
